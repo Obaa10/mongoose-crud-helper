39bcd62d84b715cb9e2ec043d65a89e132b12904
obada <obada.maleh@gmail.com>
2024-03-24 11:25:29 +0300

    Create router.js

---
+++ src/router.js
@@ -0,0 +1,26 @@
+import Crud from "./crud.controller.js";
+import express from "express";
+
+/**
+ * @param className
+ * @param {Array}  setMiddlewares
+ * @param {Array}  getMiddlewares
+ */
+
+export default ({ className, setMiddlewares, getMiddlewares }) => {
+  const router = express.Router();
+  const crud = new Crud(className);
+
+  router
+    .route("/")
+    .post(setMiddlewares, crud.create())
+    .get(getMiddlewares, crud.getAll());
+
+  router
+    .route("/:id")
+    .put(setMiddlewares, crud.update())
+    .get(getMiddlewares, crud.getOne())
+    .delete(setMiddlewares, crud.delete());
+
+  return router;
+};
